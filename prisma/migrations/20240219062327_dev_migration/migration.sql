7a90eea989c3214e966fbc503ad17895f661e35e
mws75 <mwspencer75@gmail.com>
2024-02-19 14:13:26 -0800

    first commit

---
+++ prisma/migrations/20240219062327_dev_migration/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateIndex
+CREATE INDEX "Users_name_idx" ON "Users"("name");
